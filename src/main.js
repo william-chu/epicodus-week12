e20d552e31125e1f9dd2a43ff18dd9114fc892f5
Student <example@example.com>
2018-06-06 09:42:10 -0700

    add logo to modals and hide content when main head buttons activated
    Former-commit-id: 9baf24dea40780ba1fad4abefe8ff31847d8dae2

--- src/main.js
+++ src/main.js
@@ -11,30 +11,41 @@ const searchModal = document.getElementById('search-modal');
 const searchBtn = document.getElementById('search-button');
 const closeModal = document.getElementsByClassName("close-modal");
 const mainContainer = document.getElementById('main-container');
+const mainLogo = document.getElementById('logo-primary');
 
-// hideMainBtnDisplay() {
-//   menuBtn.className = "main-head-button hide";
-//   searchBtn.className = "main-head-button hide";
-// }
+function hideMainBtnDisplay() {
+  mainLogo.className = "logo-primary hide";
+  menuBtn.className = "main-head-button hide";
+  searchBtn.className = "main-head-button hide";
+}
+
+function showMainBtnDisplay() {
+  mainLogo.className = "logo-primary";
+  menuBtn.className = "main-head-button";
+  searchBtn.className = "main-head-button";
+}
 
 // When the user clicks on the button, open the modal
 menuBtn.onclick = function() {
   menuModal.style.display = "block";
   mainContainer.className = "blur";
   menuBtn.className="main-head-button"
+  hideMainBtnDisplay();
 }
 searchBtn.onclick = function() {
   searchModal.style.display = "block";
   mainContainer.className = "blur";
+  hideMainBtnDisplay();
 }
 
 // When the user clicks on <span> (x), close the modal
 closeModal[0].onclick = function() {
   menuModal.style.display = "none";
   mainContainer.className = "";
+  showMainBtnDisplay();
 }
 closeModal[1].onclick = function() {
   searchModal.style.display = "none";
   mainContainer.className = "";
-
+  showMainBtnDisplay();
 }
