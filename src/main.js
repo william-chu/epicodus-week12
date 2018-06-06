6390224b0f81882eb1f4cffa4442f9a6b0c8194e
Student <example@example.com>
2018-06-06 08:48:51 -0700

    add modals for menu and search buttons
    Former-commit-id: 7ceab740e672fd021bbb85a6f16dd4e0f1f7881f

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
 import './input.scss';
-import $ from 'jquery';
+// import $ from 'jquery';
+//
+// $(document).ready(function() {
+//
+// });
 
-$(document).ready(function() {
+const menuModal = document.getElementById('menu-modal');
+const menuBtn = document.getElementById("menu-button");
+const searchModal = document.getElementById('search-modal');
+const searchBtn = document.getElementById('search-button');
+const closeModal = document.getElementsByClassName("close-modal");
+const mainContainer = document.getElementById('main-container');
 
-});
+// hideMainBtnDisplay() {
+//   menuBtn.className = "main-head-button hide";
+//   searchBtn.className = "main-head-button hide";
+// }
+
+// When the user clicks on the button, open the modal
+menuBtn.onclick = function() {
+  menuModal.style.display = "block";
+  mainContainer.className = "blur";
+  menuBtn.className="main-head-button"
+}
+searchBtn.onclick = function() {
+  searchModal.style.display = "block";
+  mainContainer.className = "blur";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeModal[0].onclick = function() {
+  menuModal.style.display = "none";
+  mainContainer.className = "";
+}
+closeModal[1].onclick = function() {
+  searchModal.style.display = "none";
+  mainContainer.className = "";
+
+}
